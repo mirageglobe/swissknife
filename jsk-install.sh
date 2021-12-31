aecec075f7b58870b81da159293410c4262b2dca
Jimmy Lim <mirageglobe@gmail.com>
2021-12-31 15:18:44 +0000

    clear message

--- jsk-install.sh
+++ jsk-install.sh
@@ -4,17 +4,19 @@
 
 # author      : jimmy mg lim (mirageglobe@gmail.com)
 # source      : https://github.com/mirageglobe/swissknife
-# version     : 0.1.0
+# version     : 0.2.0
 
 # --------------------------------------------------------------------- main ---
 
-# this is an example script to install into /usr/local/bin (which is normally in your path)
+# this is an example script to install an application into /usr/local/bin
+# /usr/local/bin/ is normally in your $PATH
 # it installs either from local cloned repo or remote github service
 
 # installs samurai into /usr/local/bin
 # files: samurai / samurai-mac.py / samurai-linux.py
 
 readonly SUDO_REQUIRED=true
+readonly APP_NAME=samurai
 
 if [[ $SUDO_REQUIRED ]]; then
   if [[ $(id -u) != 0 ]]; then
@@ -27,7 +29,7 @@ if [[ $SUDO_REQUIRED ]]; then
   fi
 fi
 
-if git ls-files >& /dev/null && [[ -f samurai ]]; then
+if git ls-files >& /dev/null && [[ -f samurai.py ]]; then
   # install from local repo
   $SUDO cp ./samurai /usr/local/bin/samurai || { echo "failed to install samurai into /usr/local/bin."; exit 1; }
   $SUDO cp ./samurai-mac.py /usr/local/bin/samurai-mac.py || { echo "failed to install samurai-mac.py into /usr/local/bin."; exit 1; }
@@ -39,4 +41,6 @@ else
   $SUDO curl -L https://raw.githubusercontent.com/mirageglobe/samurai/master/samurai-linux.py -o /usr/local/bin/samurai-linux.py && $SUDO chmod +x /usr/local/bin/samurai-linux.py || { echo "failed to install samurai-linux.py into /usr/local/bin."; exit 1; }
 fi
 
-echo "Installed Samurai into /usr/local/bin. To uninstall, delete samurai/samurai-mac.py/samurai-linux.py from folder."; exit 0;
+echo "installed $APP_NAME into /usr/local/bin.";
+echo "to uninstall, delete samurai / samurai-mac.py / samurai-linux.py from folder /usr/local/bin";
+exit 0;
